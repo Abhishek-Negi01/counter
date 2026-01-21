adbfca6648debe978e325d2ce47f04f92ab1f630
Abhishek Negi <negiabhishek75054@gmail.com>
2026-01-21 21:29:53 +0530

    did setup of server entry point and express app

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { app } from "./src/app.js";
+import connectDB from "./src/db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error ", err);
+  });
---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+app.use(cookieParser());
+
+// Routers and utils
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import { errorHandler } from "./middlewares/error.middlewares.js";
+
+// routes
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.use(errorHandler);
+export { app };
